aa64cddb8f8588e3cdd97ad5f7af821d5fd69a36
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2015-12-29 02:43:40 +0200

    ptrcmp

---
+++ lib/ptrcmp.hh
@@ -0,0 +1,26 @@
+#ifndef CLICK_CLICKITYCLACK_PTRCMP_HH
+#define CLICK_CLICKITYCLACK_PTRCMP_HH
+
+#include <click/config.h>
+#include <click/glue.hh>
+
+CLICK_DECLS
+
+namespace ClickityClack
+{
+
+int ptrCmp(const void *aPtr, const void *bPtr)
+{
+	uint64_t a = reinterpret_cast<uint64_t>(aPtr);
+	uint64_t b = reinterpret_cast<uint64_t>(bPtr);
+	
+	if (a < b)
+		return -1;
+	if (a > b)
+		return 1;
+	return 0;
+}
+
+CLICK_ENDDECLS
+
+#endif /* CLICK_CLICKITYCLACK_PTRCMP_HH */

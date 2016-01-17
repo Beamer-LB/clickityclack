a0c9fd39b7c10146f4b7ebf5274b013e5e6d8895
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2016-01-17 20:47:47 +0200

    staticMemcpy: force inline

--- lib/staticmemcpy.hh
+++ lib/staticmemcpy.hh
@@ -9,7 +9,7 @@ CLICK_DECLS
 namespace ClickityClack
 {
 
-template <typename T> void intTransfer(void *dst, void *src)
+template <typename T> __attribute__((always_inline)) void intTransfer(void *dst, void *src)
 {
 	T *dstInt = (T *)dst;
 	T *srcInt = (T *)src;
@@ -18,64 +18,64 @@ template <typename T> void intTransfer(void *dst, void *src)
 }
 
 /* general case */
-template <size_t SIZE> inline void staticMemcpy(char *dst, char *src)
+template <size_t SIZE> __attribute__((always_inline)) void staticMemcpy(char *dst, char *src)
 {
 	staticMemcpy<8>(dst, src);
 	staticMemcpy<SIZE - 8>(dst + 8, src + 8);
 }
 
-template <size_t SIZE> inline void staticMemcpy(void *dst, void *src)\
+template <size_t SIZE> __attribute__((always_inline)) void staticMemcpy(void *dst, void *src)\
 {
 	staticMemcpy<SIZE>((char *)dst, (char *)src);
 }
 
 /* one value at a time */
-template <> inline void staticMemcpy<8>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<8>(char *dst, char *src)
 {
 	intTransfer<uint64_t>(dst, src);
 }
 
-template <> inline void staticMemcpy<4>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<4>(char *dst, char *src)
 {
 	intTransfer<uint32_t>(dst, src);
 }
 
-template <> inline void staticMemcpy<2>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<2>(char *dst, char *src)
 {
 	intTransfer<uint16_t>(dst, src);
 }
 
-template <> inline void staticMemcpy<1>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<1>(char *dst, char *src)
 {
 	intTransfer<uint8_t>(dst, src);
 }
 
-template <> inline void staticMemcpy<0>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<0>(char *dst, char *src)
 {
 	(void)dst;
 	(void)src;
 }
 
 /* two values at a time */
-template <> inline void staticMemcpy<3>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<3>(char *dst, char *src)
 {
 	staticMemcpy<2>(dst, src);
 	staticMemcpy<1>(dst + 2, src + 2);
 }
 
-template <> inline void staticMemcpy<5>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<5>(char *dst, char *src)
 {
 	staticMemcpy<4>(dst, src);
 	staticMemcpy<1>(dst + 4, src + 4);
 }
 
-template <> inline void staticMemcpy<6>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<6>(char *dst, char *src)
 {
 	staticMemcpy<4>(dst, src);
 	staticMemcpy<2>(dst + 4, src + 4);
 }
 
-template <> inline void staticMemcpy<7>(char *dst, char *src)
+template <> __attribute__((always_inline)) void staticMemcpy<7>(char *dst, char *src)
 {
 	staticMemcpy<4>(dst, src);
 	staticMemcpy<3>(dst + 4, src + 4);

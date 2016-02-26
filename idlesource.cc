583c1dbffb420af87d74c5d207f6f48bb31cd0d3
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2016-02-26 17:13:29 +0200

    IdleSource

---
+++ idlesource.cc
@@ -0,0 +1,7 @@
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/args.hh>
+#include "idlesource.hh"
+
+EXPORT_ELEMENT(IdleSource)
---
+++ idlesource.hh
@@ -0,0 +1,33 @@
+#ifndef CLICK_IDLESOURCE_HH
+#define CLICK_IDLESOURCE_HH
+
+#include <click/config.h>
+#include <click/element.hh>
+#if HAVE_BATCH
+#include <click/batchelement.hh>
+#endif
+
+
+CLICK_DECLS
+
+#if HAVE_BATCH
+class IdleSource: public BatchElement
+#else
+class IdleSource: public Element
+#endif
+{
+public:
+	IdleSource() {}
+
+	~IdleSource() {}
+
+	const char *class_name() const { return "IdleSource"; }
+
+	const char *port_count() const { return "0/1"; }
+
+	const char *processing() const { return PUSH; }
+};
+
+CLICK_ENDDECLS
+
+#endif /* CLICK_IDLESOURCE_HH */

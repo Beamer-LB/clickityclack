ae56fb6e6e8f9545d7140aa7156376720fe239a1
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2016-04-13 17:53:39 +0300

    PPSReporter

---
+++ ppsreporter.cc
@@ -0,0 +1,79 @@
+#include <click/config.h>
+#include <click/confparse.hh>
+#include <click/error.hh>
+#include <click/args.hh>
+#include "ppsreporter.hh"
+
+CLICK_DECLS
+
+int PPSReporter::configure(Vector<String> &conf, ErrorHandler *errh)
+{
+	epochSize = 1000000;
+	
+	seen = new int[click_max_cpu_ids()]; assert(seen);
+	lastEpoch = new click_jiffies_t[click_max_cpu_ids()]; assert(lastEpoch);
+	for (int i = 0; i < click_max_cpu_ids(); i++)
+	{
+		seen[i] = 0;
+		lastEpoch[i] = click_jiffies();
+	}
+	
+	if (Args(conf, this, errh)
+		.read("EPOCH", IntArg(), epochSize)
+		.complete() < 0)
+	{
+		return -1;
+	}
+	
+	return 0;
+}
+
+static inline void report(int thread, click_jiffies_t duration)
+{
+	click_chatter("%d: %d", thread, (int)duration);
+}
+
+#if HAVE_BATCH
+PacketBatch *PPSReporter::simple_action_batch(PacketBatch *head)
+{
+	Packet* current = head;
+	Packet* last = head;
+	while (current != NULL)
+	{
+		/* do stuff */
+		Packet *result = simple_action(current);
+		
+		if (current == head)
+		{
+			head = PacketBatch::start_head(result);
+			head->set_next(current->next());
+		}
+		else
+		{
+			last->set_next(result);
+			result->set_next(current->next());
+		}
+		
+		last = result;
+		current = result->next();
+	}
+	return head;
+}
+#endif
+
+Packet *PPSReporter::simple_action(Packet *p)
+{
+	int thread = click_current_cpu_id();
+	
+	seen[thread]++;
+	
+	if (unlikely(seen[thread] >= epochSize))
+		newEpoch(thread);
+	
+	return p;
+}
+
+CLICK_ENDDECLS
+
+EXPORT_ELEMENT(PPSReporter)
+
---
+++ ppsreporter.hh
@@ -0,0 +1,57 @@
+#ifndef CLICK_PPSREPORTER_HH
+#define CLICK_PPSREPORTER_HH
+
+#include <click/config.h>
+#include <click/element.hh>
+#include <click/etheraddress.hh>
+#include <click/ipaddress.hh>
+#if HAVE_BATCH
+#include <click/batchelement.hh>
+#endif
+#include "../clickityclack/lib/macint64.hh"
+
+CLICK_DECLS
+
+#if HAVE_BATCH
+class PPSReporter: public BatchElement
+#else
+class PPSReporter: public Element
+#endif
+{
+	int epochSize;
+	
+	int *seen;
+	click_jiffies_t *lastEpoch;
+	
+	inline void newEpoch(int thread)
+	{
+		click_jiffies_t now = click_jiffies();
+		
+		seen[thread] -= epochSize;
+		click_chatter("%d: %d", thread, (int)(lastEpoch[thread] - now));
+		lastEpoch[thread] = now;
+	}
+	
+public:
+	PPSReporter() {}
+	
+	~PPSReporter() {}
+	
+	const char *class_name() const { return "PPSReporter"; }
+	
+	const char *port_count() const { return "1/1"; }
+	
+	const char *processing() const { return AGNOSTIC; }
+	
+	int configure(Vector<String> &conf, ErrorHandler *errh);
+	
+	Packet *simple_action(Packet *p);
+	
+#if HAVE_BATCH
+	PacketBatch *simple_action_batch(PacketBatch *head);
+#endif
+};
+
+CLICK_ENDDECLS
+
+#endif /* CLICK_PPSREPORTER_HH */
